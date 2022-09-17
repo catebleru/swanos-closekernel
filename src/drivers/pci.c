90a64d0bb3d660c3ec8c3f68784a4a27cdac9aa8
Daniil Lebedev <93409280+holu31@users.noreply.github.com>
2022-09-17 15:07:33 +0300

    Update pci.c

--- src/drivers/pci.c
+++ src/drivers/pci.c
@@ -4,6 +4,8 @@
 #include <console.h>
 
 
+// IMPORTANT: MUST REDO
+
 uint16_t pci_read_word(uint8_t bus, uint8_t slot, uint8_t func, uint8_t offset){
     uint32_t address;
     uint32_t lbus  = (uint32_t)bus;
